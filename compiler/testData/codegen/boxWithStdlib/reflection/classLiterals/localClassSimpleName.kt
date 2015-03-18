805933788bb5127236f9f6d9b73556c3afabaea5
Alexander Udalov <Alexander.Udalov@jetbrains.com>
2015-03-18 17:14:13 +0300

    Fix KClass.simpleName for local classes

---
+++ compiler/testData/codegen/boxWithStdlib/reflection/classLiterals/localClassSimpleName.kt
@@ -0,0 +1,30 @@
+import kotlin.reflect.KClass
+import kotlin.test.assertEquals
+
+fun check(klass: KClass<*>, expectedName: String) {
+    assertEquals(expectedName, klass.simpleName)
+}
+
+fun localInMethod() {
+    class Local
+    check(Local::class, "Local")
+
+    class `Local$With$Dollars`
+    check(`Local$With$Dollars`::class, "Local\$With\$Dollars")
+}
+
+class LocalInConstructor {
+    init {
+        class Local
+        check(Local::class, "Local")
+
+        class `Local$With$Dollars`
+        check(`Local$With$Dollars`::class, "Local\$With\$Dollars")
+    }
+}
+
+fun box(): String {
+    localInMethod()
+    LocalInConstructor()
+    return "OK"
+}
