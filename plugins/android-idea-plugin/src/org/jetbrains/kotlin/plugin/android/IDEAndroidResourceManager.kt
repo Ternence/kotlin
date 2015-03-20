74b280eae48e6d2dc411441ceee4933e5b09ac2a
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2015-03-20 22:20:28 +0300

    Lookup XML attribute using the property (for a particular layout)

--- plugins/android-idea-plugin/src/org/jetbrains/kotlin/plugin/android/IDEAndroidResourceManager.kt
+++ plugins/android-idea-plugin/src/org/jetbrains/kotlin/plugin/android/IDEAndroidResourceManager.kt
@@ -23,18 +23,28 @@ import kotlin.properties.Delegates
 import org.jetbrains.kotlin.plugin.android.AndroidXmlVisitor
 import org.jetbrains.kotlin.lang.resolve.android.AndroidResourceManager
 import org.jetbrains.kotlin.lang.resolve.android.AndroidModuleInfo
+import org.jetbrains.kotlin.psi.JetProperty
 
 public class IDEAndroidResourceManager(val module: Module) : AndroidResourceManager(module.getProject()) {
 
     override val androidModuleInfo: AndroidModuleInfo? by Delegates.lazy { module.androidFacet?.toAndroidModuleInfo() }
 
-    override fun idToXmlAttribute(id: String): PsiElement? {
+    override fun propertyToXmlAttribute(property: JetProperty): PsiElement? {
+        val fqPath = property.getFqName()?.pathSegments() ?: return null
+        if (fqPath.size() < 2) return null
+
+        val layoutPackageName = fqPath[fqPath.size() - 2].asString()
+        val layoutFiles = getLayoutXmlFiles()[layoutPackageName]
+        if (layoutFiles == null || layoutFiles.isEmpty()) return null
+
+        val propertyName = property.getName()
+
         var ret: PsiElement? = null
-        for (file in getLayoutXmlFiles().values().flatMap { it }) {
-            file.accept(AndroidXmlVisitor({ retId, wClass, valueElement ->
-                if (retId == id) ret = valueElement
-            }))
+        val visitor = AndroidXmlVisitor { retId, wClass, valueElement ->
+            if (retId == propertyName) ret = valueElement
         }
+
+        layoutFiles.first().accept(visitor)
         return ret
     }
 
