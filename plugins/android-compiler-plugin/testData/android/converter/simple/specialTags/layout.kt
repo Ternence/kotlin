1a635791264ae51a387526fb6117959341400cd1
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2015-03-25 17:15:09 +0300

    Ignore some XML tags

--- plugins/android-compiler-plugin/testData/android/converter/simple/specialTags/layout.kt
+++ plugins/android-compiler-plugin/testData/android/converter/simple/specialTags/layout.kt
@@ -1,21 +1,21 @@
 package kotlinx.android.synthetic.layout
 
-import android.app.Activity
-import android.app.Fragment
-import android.view.View
+import android.app.*
+import android.view.*
 import android.widget.*
+import android.webkit.*
+import android.inputmethodservice.*
+import android.opengl.*
+import android.appwidget.*
+import android.support.v4.app.*
+import android.support.v4.view.*
+import android.support.v4.widget.*
 import kotlin.internal.flexible.ft
 
 val Activity.includeTag: ft<View, View?>
     get() = findViewById(0) : View
 
 val Fragment.includeTag: ft<View, View?>
-    get() = getView().findViewById(0) : View
-
-val Activity.mergeTag: ft<View, View?>
-    get() = findViewById(0) : View
-
-val Fragment.mergeTag: ft<View, View?>
     get() = getView().findViewById(0) : View
 
 val Activity.fragmentTag: ft<View, View?>
--- plugins/android-compiler-plugin/testData/android/converter/simple/specialTags/layout1.kt
+++ plugins/android-compiler-plugin/testData/android/converter/simple/specialTags/layout1.kt
@@ -1,15 +1,18 @@
 package kotlinx.android.synthetic.layout.view
 
-import android.app.Activity
-import android.app.Fragment
-import android.view.View
+import android.app.*
+import android.view.*
 import android.widget.*
+import android.webkit.*
+import android.inputmethodservice.*
+import android.opengl.*
+import android.appwidget.*
+import android.support.v4.app.*
+import android.support.v4.view.*
+import android.support.v4.widget.*
 import kotlin.internal.flexible.ft
 
 val View.includeTag: ft<View, View?>
-    get() = findViewById(0) : View
-
-val View.mergeTag: ft<View, View?>
     get() = findViewById(0) : View
 
 val View.fragmentTag: ft<View, View?>
